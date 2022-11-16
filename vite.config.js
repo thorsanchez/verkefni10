46c380a558cd93383cbc70135f0e75b2640651b5
thorsanchez <ths281@hi.is>
2022-11-16 21:02:28 +0000

    kk

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,2 @@
-const { defineConfig } = require('vite')
 
-module.exports = defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        about: 'whereuat.html',
-        shaderOne: 'main.js',
-        tralla: 'file.js',
-        Animation: 'counter.js',
-        // ...
-        // List all files you want in your build
-      }
-    }
-  }
-})
 
