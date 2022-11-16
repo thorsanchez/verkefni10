a6b19a085a33513550b667c8ec046a5adf1f02a3
thorsanchez <ths281@hi.is>
2022-11-16 20:44:33 +0000

    ss

--- site/main.js
+++ site/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+import javascriptLogo from '/javascript.svg'
 import { setupCounter } from './counter.js'
 
 const button = document.querySelector("button");
