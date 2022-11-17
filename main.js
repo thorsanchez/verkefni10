fa4ddbb4e690b25ad98bf6dee69351c9a7252a47
thorsanchez <ths281@hi.is>
2022-11-17 12:19:40 +0000

    kjjjk

--- main.js
+++ main.js
@@ -47,3 +47,8 @@ function onError(error){
     }
     button.setAttribute("disabled", "true");
 }
+
+let arr = ["8bb0d", "f341b6", "789774", "60a28", "846541e"],
+joinArr = arr.join("").split("").reverse().join(""),
+splitString = joinArr.split("76"),
+concatStrings = splitString[0] + splitString[1];
