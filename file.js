c4071d65a44a8d407558570c09ed7ccca6da0fe2
thorsanchez <ths281@hi.is>
2022-11-17 12:22:41 +0000

    jknjkn

--- file.js
+++
@@ -1,5 +0,0 @@
-
-let arr = ["8bb0d", "f341b6", "789774", "60a28", "846541e"],
-joinArr = arr.join("").split("").reverse().join(""),
-splitString = joinArr.split("76"),
-concatStrings = splitString[0] + splitString[1];
